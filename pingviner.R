eb0e50d9380763bcaff49c96f01a39f98ad3d649
JensUstvedt <jens.ustvedt100@gmail.com>
2025-03-31 11:11:56 +0200

    Added lines to the two plots

--- pingviner.R
+++ pingviner.R
@@ -4,10 +4,12 @@ library(palmerpenguins)
 
 
 island_plot <- ggplot(data = pingvin_data, aes(x = body_mass_g, y = bill_length_mm, colour = island)) +
-  geom_point()
+  geom_point() +
+  geom_line()
 
 species_plot <- ggplot(data = pingvin_data, aes(x = body_mass_g, y = bill_length_mm, colour = species)) +
-         geom_point()
+         geom_point() +
+  geom_line()
 
 island_plot
 species_plot
